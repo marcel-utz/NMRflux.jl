6fbcf93ad4bb80aa5023267b85187be702790c6b
Marcel Utz <marcel.utz@gmx.net>
2026-03-16 10:49:36 +0100

    made MedianBaseline a 1D processor

--- test/runtests.jl
+++ test/runtests.jl
@@ -21,3 +21,16 @@ end
     @test size(d) == size(d.coord[1])
     @test isapprox(2.616357072520625e11 + 1.335611666755e11im,sum(d))
 end
+
+@testset "NMRlab.jl processing" begin
+    # Write your tests here.
+    ex=NMRlab.Examples.Data["HCC cell culture media spectra"]["files"][1]
+    params,d=NMRlab.load(ex,:Bruker)
+    zf = ZeroFill([2^16])
+    ft = FourierTransform([2^16],[1])
+    pc = PhaseCorrect(0.80pi,2pi*0.00172,1)
+    bc = MedianBaselineCorrect(1,wdw=1024)
+    ftdat = d |> zf |> ft |> pc |> bc
+    @test size(ftdat) == size(ftdat.coord[1])
+end
+
